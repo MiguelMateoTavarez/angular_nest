a9947133cef362b3953f855cd6ec7d07071445c5
Miguel Mateo <miguelmateot94@gmail.com>
2024-09-17 15:21:16 -0400

    feat: modules created and configured

---
+++ authApp/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardLayoutComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class DashboardModule { }
