032576beaccb25315be5f69acc311657c158d3c7
Miguel Mateo <miguelmateot94@gmail.com>
2024-09-17 21:00:15 -0400

    feat: build interfaces and enums

--- authApp/src/app/auth/services/auth.service.ts
+++ authApp/src/app/auth/services/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { environments } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { User } from '../interfaces';
+import { AuthStatus } from '../enums';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environments.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
   constructor() { }
+
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
