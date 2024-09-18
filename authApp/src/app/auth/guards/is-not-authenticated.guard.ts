fc0b9dcfc9834cb1f4953441a65ff11d6931237b
Miguel Mateo <miguelmateot94@gmail.com>
2024-09-18 16:12:43 -0400

    feat: add is not authenticated guard

--- authApp/src/app/auth/guards/is-not-authenticated.guard.ts
+++ authApp/src/app/auth/guards/is-not-authenticated.guard.ts
@@ -1,20 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 import { AuthStatus } from '../enums';
+import { AuthService } from '../services/auth.service';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
 
-  if(authService.authStatus() === AuthStatus.authenticated){
+  if(authService.authStatus() === AuthStatus.notAuthenticated){
     return true;
   }
 
   // const url = state.url;
   // localStorage.setItem('url', url);
-  router.navigateByUrl('/auth/login');
+  router.navigateByUrl('/dashboard');
   return false;
 };
